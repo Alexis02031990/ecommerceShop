cee94ac0168ac5b79ecf950feae8ce93dfb57ff4
Alexis02031990 <alexis_writemail@yahoo.com>
2024-02-08 16:06:46 +0100

    Ajoutez des fichiers projet.

---
+++ API/Controllers/ProductsController.cs
@@ -0,0 +1,53 @@
+﻿using API.Data;
+using API.EntitiesModels;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly StoreContext _context;
+
+        public ProductsController(StoreContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Product>>> GetProducts()
+        {
+            var products = await _context.Products.ToListAsync();
+
+            return Ok(products);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Product>> GetProductById(int id)
+        {
+            return await _context.Products.FindAsync(id);
+        }
+
+        [HttpPost]
+        public string CreateProduct()
+        {
+
+            return "";
+        }
+
+        [HttpPost("Id")]
+        public string UpdateProduct(int id)
+        {
+            return "";
+        }
+
+        [HttpDelete("Id")]
+        public void DeleteProduct(int id)
+        {
+
+        }
+    }
+}
---
+++ API/EntitiesModels/Product.cs
@@ -0,0 +1,14 @@
+﻿namespace API.EntitiesModels
+{
+    public class Product
+    {
+        public int Id { get; set; }
+
+        public string? Name { get; set; }
+
+        public string? Description { get; set; }
+
+        public string? image { get; set; }
+
+    }
+}
